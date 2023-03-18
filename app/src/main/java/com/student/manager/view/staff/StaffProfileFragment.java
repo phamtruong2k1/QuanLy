e658ef816bd51153167bc86c8596fdc3da6cdcd0
Phạm Ánh Trường <61191253+phamtruong2k1@users.noreply.github.com>
2023-03-18 14:57:43 +0700

    commit 15

--- app/src/main/java/com/student/manager/view/staff/StaffProfileFragment.java
+++ app/src/main/java/com/student/manager/view/staff/StaffProfileFragment.java
@@ -9,9 +9,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.student.manager.R;
+import com.student.manager.databinding.FragmentLecturerProfileBinding;
+import com.student.manager.databinding.FragmentStaffProfileBinding;
 import com.student.manager.model.Staff;
 
 public class StaffProfileFragment extends Fragment {
+
+    FragmentStaffProfileBinding binding;
 
     Staff staff;
 
@@ -29,7 +33,19 @@ public class StaffProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_staff_profile, container, false);
+        binding = FragmentStaffProfileBinding.inflate(inflater, container, false);
+
+        binding.edtName.setText(staff.getName());
+        binding.edtDateOfBirth.setText(staff.getDate_of_birth());
+        binding.edtAddress.setText(staff.getAddress());
+        binding.edtPhone.setText(staff.getPhone_number());
+
+        if (staff.getGender() == 1) {
+            binding.rdgGender.setText("Male");
+        } else {
+            binding.rdgGender.setText("Female");
+        }
+
+        return binding.getRoot();
     }
 }
